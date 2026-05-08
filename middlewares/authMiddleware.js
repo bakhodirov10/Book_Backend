ef5d99aa20e715fc464e021efd95cd2a57eff344
Bakhodirov Abdulloh <boxodirovabdulloh46@gmail.com>
2026-05-08 18:05:20 +0500

    hatoliktuzalmoqda shekil

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,18 +1,12 @@
 const jwt = require("jsonwebtoken")
 
-exports.authMiddleware = (req, res, next) =>{
-    const authHeader = req.header.authMiddleware
-
-    if(!authHeader){
-        res.status(401).json({
-            message: "Token required"
-        })
+exports.authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).json({ message: "Token required" })
     }
-
     const token = authHeader.split(" ")[1]
     const decoded = jwt.verify(token, process.env.JWT_TOKEN)
-
     req.user = decoded
-
     next()
 }
